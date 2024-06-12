7c48a7b40d9451260962eee32741a81917c335a8
Evgeny Romanov <e.romanov93@yandex.ru>
2024-06-12 21:10:20 +0500

    HW3 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,54 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+const TopCount = 10
+
+type Word struct {
+	Word string
+	Cnt  int
+}
+
+func Top10(text string) []string {
+	testSplit := strings.Fields(text)
+	wordMap := make(map[string]int)
+	wordSlice := make([]Word, 0, TopCount)
+	result := make([]string, 0, TopCount)
+
+	for _, word := range testSplit {
+		_, ok := wordMap[word]
+
+		if ok {
+			wordMap[word]++
+		} else {
+			wordMap[word] = 1
+		}
+	}
+
+	for word, cnt := range wordMap {
+		wordSlice = append(wordSlice, Word{Word: word, Cnt: cnt})
+	}
+
+	sort.Slice(wordSlice, func(i, j int) bool {
+		if wordSlice[i].Cnt == wordSlice[j].Cnt {
+			return wordSlice[i].Word < wordSlice[j].Word
+		}
+		return wordSlice[i].Cnt > wordSlice[j].Cnt
+	})
+
+	topBorder := TopCount
+	lenWordSlice := len(wordSlice)
+
+	if len(wordSlice) < TopCount {
+		topBorder = lenWordSlice
+	}
+
+	for _, word := range wordSlice[:topBorder] {
+		result = append(result, word.Word)
+	}
+
+	return result
 }
