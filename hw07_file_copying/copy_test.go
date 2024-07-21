73dd2081433cd8b08d5fe8f27e9f08f63f4ecde4
Evgeny Romanov <e.romanov93@yandex.ru>
2024-07-22 00:59:21 +0500

    HW7 is completed

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,91 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
 	// Place your code here.
+	for _, tcase := range [...]struct {
+		in       string
+		out      string
+		expected string
+		offset   int64
+		limit    int64
+	}{
+		{
+			in:       "./testdata/input.txt",
+			out:      "out_offset0_limit0.txt",
+			expected: "./testdata/out_offset0_limit0.txt",
+			offset:   0,
+			limit:    0,
+		},
+		{
+			in:       "./testdata/input.txt",
+			out:      "out_offset0_limit10.txt",
+			expected: "./testdata/out_offset0_limit10.txt",
+			offset:   0,
+			limit:    10,
+		},
+		{
+			in:       "./testdata/input.txt",
+			out:      "out_offset0_limit1000.txt",
+			expected: "./testdata/out_offset0_limit1000.txt",
+			offset:   0,
+			limit:    1000,
+		},
+		{
+			in:       "./testdata/input.txt",
+			out:      "out_offset0_limit10000.txt",
+			expected: "./testdata/out_offset0_limit10000.txt",
+			offset:   0,
+			limit:    10000,
+		},
+		{
+			in:       "./testdata/input.txt",
+			out:      "out_offset100_limit1000.txt",
+			expected: "./testdata/out_offset100_limit1000.txt",
+			offset:   100,
+			limit:    1000,
+		},
+		{
+			in:       "./testdata/input.txt",
+			out:      "out_offset6000_limit1000.txt",
+			expected: "./testdata/out_offset6000_limit1000.txt",
+			offset:   6000,
+			limit:    1000,
+		},
+	} {
+		t.Run(fmt.Sprintf("test-%q", tcase.out), func(t *testing.T) {
+			f, _ := os.CreateTemp("", tcase.out)
+			defer func(name string) {
+				_ = os.Remove(name)
+			}(f.Name())
+
+			_ = Copy(tcase.in, f.Name(), tcase.offset, tcase.limit)
+			out, _ := os.ReadFile(f.Name())
+			expected, _ := os.ReadFile(tcase.expected)
+
+			if !bytes.Equal(out, expected) {
+				t.Errorf("incoming file and outcomming file not matched")
+			}
+		})
+	}
+
+	t.Run("Unsupported file", func(t *testing.T) {
+		err := Copy("/dev/urandom", "output.txt", 0, 0)
+		expected := ErrUnsupportedFile
+		require.Equal(t, expected, err)
+	})
+
+	t.Run("Offset exceeds file size", func(t *testing.T) {
+		err := Copy("./testdata/input.txt", "output.txt", 10000, 50)
+		expected := ErrOffsetExceedsFileSize
+		require.Equal(t, expected, err)
+	})
 }
