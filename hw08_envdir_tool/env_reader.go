bb620488b9fded0ade848ed8cac9838491646e16
Evgeny Romanov <e.romanov93@yandex.ru>
2024-07-27 21:30:10 +0500

    HW8 is completed

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,5 +1,23 @@
 package main
 
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"io"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+const (
+	invalidSymbol string = "="
+	terminateByte byte   = 0x00
+	lineFeed      byte   = '\n'
+	trimRightSet         = "\t "
+)
+
 type Environment map[string]EnvValue
 
 // EnvValue helps to distinguish between empty files and files with the first empty line.
@@ -12,5 +30,56 @@ type EnvValue struct {
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
 	// Place your code here
-	return nil, nil
+	environments := make(Environment, 10)
+
+	dirEntries, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, dirEntry := range dirEntries {
+		if dirEntry.IsDir() || !dirEntry.Type().IsRegular() || strings.Contains(dirEntry.Name(), invalidSymbol) {
+			continue
+		}
+
+		firstLineBytes, err := readLine(dir, dirEntry)
+		if err != nil {
+			return nil, err
+		}
+		firstLineString := string(bytes.ReplaceAll(firstLineBytes, []byte{terminateByte}, []byte{lineFeed}))
+		firstLineString = strings.TrimRight(firstLineString, trimRightSet)
+
+		envValue := EnvValue{
+			Value:      firstLineString,
+			NeedRemove: false,
+		}
+
+		if firstLineString == "" {
+			envValue.NeedRemove = true
+		}
+
+		environments[dirEntry.Name()] = envValue
+	}
+
+	return environments, nil
+}
+
+func readLine(dir string, dirEntry fs.DirEntry) ([]byte, error) {
+	f, err := os.OpenFile(filepath.Join(dir, dirEntry.Name()), os.O_RDONLY, 0o644)
+	if err != nil {
+		return nil, err
+	}
+	defer func(f *os.File) {
+		_ = f.Close()
+	}(f)
+
+	reader := bufio.NewReader(f)
+	line, _, err := reader.ReadLine()
+	if err != nil {
+		if !errors.Is(err, io.EOF) {
+			return nil, err
+		}
+	}
+
+	return line, nil
 }
--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,47 @@
 package main
 
+import (
+	"errors"
+	"log"
+	"os"
+	"os/exec"
+)
+
+var ErrEmptyCmd = errors.New("command is empty")
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
 func RunCmd(cmd []string, env Environment) (returnCode int) {
 	// Place your code here.
+	if len(cmd) == 0 {
+		log.Println(ErrEmptyCmd)
+		return 1
+	}
+
+	for eVar, eVal := range env {
+		if eVal.NeedRemove {
+			err := os.Unsetenv(eVar)
+			if err != nil {
+				return 1
+			}
+			continue
+		}
+
+		err := os.Setenv(eVar, eVal.Value)
+		if err != nil {
+			return 1
+		}
+	}
+
+	commandName := cmd[0]
+	args := cmd[1:]
+	proc := exec.Command(commandName, args...)
+	proc.Stdout = os.Stdout
+	proc.Stderr = os.Stderr
+	proc.Stdin = os.Stdin
+
+	if err := proc.Run(); err != nil {
+		return 1
+	}
+
 	return
 }
