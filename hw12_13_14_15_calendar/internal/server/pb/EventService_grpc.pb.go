1761c72cec8afe303fc8b84e5bf0e0bfa327e761
Evgeny Romanov <e.romanov93@yandex.ru>
2025-01-16 08:37:18 +0500

    grpc-хандлеры

--- hw12_13_14_15_calendar/internal/server/pb/EventService_grpc.pb.go
+++ hw12_13_14_15_calendar/internal/server/pb/EventService_grpc.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // - protoc-gen-go-grpc v1.5.1
 // - protoc             v3.21.12
-// source: api/EventService.proto
+// source: EventService.proto
 
 package pb
 
@@ -384,5 +384,5 @@ var CalendarService_ServiceDesc = grpc.ServiceDesc{
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
-	Metadata: "api/EventService.proto",
+	Metadata: "EventService.proto",
 }
