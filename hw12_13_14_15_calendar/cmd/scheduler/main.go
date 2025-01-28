58bf38a917ac54504a905528cd6db432a420470a
Evgeny Romanov <e.romanov93@yandex.ru>
2025-01-29 00:51:39 +0500

    hw14_calendar

---
+++ hw12_13_14_15_calendar/cmd/scheduler/main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"github.com/EvgenyRomanov/otus_go_hw/hw12_13_14_15_calendar/internal/app/scheduler"
+	"github.com/EvgenyRomanov/otus_go_hw/hw12_13_14_15_calendar/internal/logger"
+	"github.com/EvgenyRomanov/otus_go_hw/hw12_13_14_15_calendar/internal/storage"
+	memorystorage "github.com/EvgenyRomanov/otus_go_hw/hw12_13_14_15_calendar/internal/storage/memory"
+	sqlstorage "github.com/EvgenyRomanov/otus_go_hw/hw12_13_14_15_calendar/internal/storage/sql"
+	"github.com/EvgenyRomanov/otus_go_hw/hw12_13_14_15_calendar/pkg/rmq"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+)
+
+var configFile string
+
+func init() {
+	flag.StringVar(&configFile, "config", "/etc/scheduler_config.toml", "Path to configuration file")
+}
+
+func main() {
+	flag.Parse()
+
+	if flag.Arg(0) == "version" {
+		printVersion()
+		return
+	}
+
+	// init context
+	ctx, cancel := signal.NotifyContext(context.Background(),
+		syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP, syscall.SIGTSTP)
+	defer cancel()
+
+	config := NewConfig()
+	logg := logger.New(config.Logger.Level, os.Stdout)
+
+	var eventStorage storage.EventStorage
+
+	if config.Storage.Driver == "postgres" {
+		connectionString := fmt.Sprintf(
+			"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+			config.DB.DBHost, config.DB.DBPort, config.DB.DBUsername, config.DB.DBPassword, config.DB.DBName,
+		)
+
+		eventStorage = sqlstorage.New(connectionString, config.Storage.MigrationsPath)
+		err := eventStorage.Connect(ctx)
+		if err != nil {
+			logg.Error("%s", "cannot connect to DB server: "+err.Error())
+			cancel()
+			os.Exit(1) //nolint:gocritic
+		}
+		defer eventStorage.Close()
+	} else {
+		eventStorage = memorystorage.New()
+	}
+
+	logg.Info(fmt.Sprintf("successfully init %s storage", config.Storage.Driver))
+
+	rmqInstance := rmq.NewRmq(
+		config.Rmq.ConsumerTag,
+		config.Rmq.URI,
+		config.Rmq.Exchange.Name,
+		config.Rmq.Exchange.Type,
+		config.Rmq.Exchange.QueueName,
+		config.Rmq.Exchange.BindingKey,
+		config.Rmq.MaxInterval,
+	)
+
+	err := rmqInstance.Connect()
+	if err != nil {
+		logg.Error("cannot connect to AMQP server: " + err.Error())
+		return
+	}
+
+	var wg sync.WaitGroup
+
+	// TODO wg.Add(1)
+	go func() {
+		<-ctx.Done()
+
+		_, cancel := context.WithTimeout(context.Background(), time.Second*3)
+		defer cancel()
+
+		if err := rmqInstance.Shutdown(); err != nil && !errors.Is(err, rmq.ErrChannelClosed) {
+			logg.Error("failed to shutdown RMQ server: " + err.Error())
+		}
+
+		logg.Info("RMQ server successfully terminated!")
+
+		wg.Done()
+	}()
+
+	scheduler := scheduler.New(
+		logg,
+		eventStorage,
+		rmqInstance,
+		config.Scheduler.RunFrequencyInterval,
+		config.Scheduler.TimeForRemoveOldEvents,
+	)
+
+	wg.Add(1)
+	go func() {
+		scheduler.NotificationSender(ctx)
+	}()
+	wg.Wait()
+}
